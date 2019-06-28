f46496c0e5099852aa1bf9a783a0c05a793a8763
Thomas Clark <tnc1997@virginmedia.com>
2019-06-28 21:24:42 +0100

    fix: jest silent reporter not found error (#7)

---
+++ samples/gulp-core-build-jest/src/subtract.ts
@@ -0,0 +1,6 @@
+/**
+ * Subtracts two numbers.
+ * @param x the first number
+ * @param y the second number
+ */
+export const subtract: (x: number, y: number) => number = (x: number, y: number): number => x - y;
